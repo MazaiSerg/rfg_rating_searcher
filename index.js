3815a41b9eb549d92f3aa366c3bb44aac2586783
Sergey <mazai.serg@gmail.com>
2021-05-10 22:21:40 +0600

    [*] index.js - исправлен Access-Control-Allow-Origin, теперь отдаем информацию всем.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const parser = require('./parser.js')
 
 app.get('/', function (req, res) {
     //res.send('Hello World')
+    res.set('Access-Control-Allow-Origin', '*')
     parser.parse(result => res.json(result))
 })
 
